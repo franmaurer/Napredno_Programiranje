1f84f504bd4af198dce3b9d1e14e88a49642f329
FranMaurer <fran.maurer1@gmail.com>
2026-01-09 16:50:48 +0100

    Adresni operator, pristup adresi i pokazivac

---
+++ 04_Polja_Pokazivači/Pristup pokazivaču.c
@@ -0,0 +1,18 @@
+/*  Adresa na kojoj se nalazi pokazivač, pristupiti ćemo
+    preko koristeći sljedeći kod:
+    
+    Odaberite jedan odgovor:
+    a) ptr
+    b) *ptr
+    c) Ništa od navedenog
+    d) &ptr
+
+    Odgovor: d)
+-----------------------------------------------------------------------
+
+
+
+
+
+
+*/
