ca8872d47f7cebb48226d9dee52cf8b7698f9b23
FranMaurer <fran.maurer1@gmail.com>
2026-01-26 22:01:22 +0100

    Velicina pokazivaca

---
+++ 07_Dinamcka_memorija/Dinamicka_tipovi_pok.c
@@ -0,0 +1,37 @@
+/*
+#include <stdio.h>
+int main()
+{
+	int a;
+	float b;
+	char c;
+	int *p=&a;
+	float *q=&b;
+	char *r=&c;
+	printf("\na=%d\nb=%d\nc=%d\n",sizeof a,sizeof b,sizeof c);
+	printf("\np=%d\nq=%d\nr=%d\n",sizeof p,sizeof q,sizeof r);
+	return 0;
+	}
+}
+
+Objasnjenje:
+
+velicina pokazivaca ne ovisi o tipu;
+na 64 bit, pokazivaci uvijek 8 byte
+
+sizeof p|sizeof q|sizeof r --> daju isti rezultat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
