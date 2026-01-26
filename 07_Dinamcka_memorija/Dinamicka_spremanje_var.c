5f73da907f24970c0bcf8081f63dcdcba992f791
FranMaurer <fran.maurer1@gmail.com>
2026-01-26 21:53:40 +0100

    Spremanje varijable

---
+++ 07_Dinamcka_memorija/Dinamicka_spremanje_var.c
@@ -0,0 +1,27 @@
+/*
+
+#include<stdio.h>
+int main(){
+	int a = 10;
+	printf("%\n%d, \n%d, \n%p, \n%x", a, sizeof a,
+	&a, sizeof &a);
+	return 0;
+}
+
+
+10, Vrijednost varijable a
+4, broj byteove koje varijabla zauzima
+0x7fffa53aa4ec, Adresa na kojoj se nalazi varijabla a
+8, broj byteove adrese na kojoj varijabla a
+
+
+
+
+
+
+
+
+
+
+
+*/
