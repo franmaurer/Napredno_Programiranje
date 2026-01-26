00afb0a1e28c380bd03b43a14b23ee567408f6c1
FranMaurer <fran.maurer1@gmail.com>
2026-01-26 22:24:53 +0100

    Drugi verzija

--- 07_Dinamcka_memorija/Dinamicka_smjestaj_objekta.c
+++ 07_Dinamcka_memorija/Dinamicka_smjestaj_objekta.c
@@ -15,12 +15,32 @@ int main(){
 Objasnjenje: Printa se 5, 6 i 7
 A krece s 5, inkrementira se te, ispisuje
 
+*************************************************************
 
+#include<stdio.h>
+int main(){
+	int i;
+	for(i=0;i<3;i++){
+		int a=5;
+		printf("%d",a);
+		a++;
+		}
+		return 0;
+		}
+	}
+}
 
+**************************************************************
+a je deklariran unutar for bloka
+a se svaki put stvara iznova
+nakon izlaska iz bloka a prestaje postojati
+printa se 5, jer je a tada toliko
 
+inkrementira se na 6 te unisti
 
+nakon toga a se opet postavi na 5
 
-
+************************************************************
 
 
 
