d98805bff9c98fe5d02a18a1b3ee0a8275b83cc5
FranMaurer <fran.maurer1@gmail.com>
2026-01-26 22:20:41 +0100

    For petlja

---
+++ 07_Dinamcka_memorija/Dinamicka_smjestaj_objekta.c
@@ -0,0 +1,34 @@
+/*
+
+#include<stdio.h>
+int main(){
+	int i;
+	int a = 5;
+	for(i=0;i<3;i++){
+		printf("%d", a);
+		a++;
+	}
+
+	return 0;
+}
+
+Objasnjenje: Printa se 5, 6 i 7
+A krece s 5, inkrementira se te, ispisuje
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
