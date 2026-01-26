9830f9d6c8b9b64ad86968f27b11a3292d4f80f6
FranMaurer <fran.maurer1@gmail.com>
2026-01-26 21:57:46 +0100

    Ispis trivijalno

---
+++ 07_Dinamcka_memorija/Dinamicka_pok.c
@@ -0,0 +1,34 @@
+/*
+
+#include<stdio.h>
+int main(){
+	int a = 1;
+	int *p = &a;
+	
+	printf("%d, %p, %d, %p", a, p, *p, &p);
+	return 0;
+}
+
+Objašnjenje: 
+
+%p -> p
+ispis: adresa od a
+primjer: 0x100
+
+%d -> *p
+ipis: vrijednost na adresi na koju p pokazuje
+to je a
+
+%p -> &p
+adresa pokazivaca
+p je takoder varijabla
+npr: 0x200
+razlicito od p
+
+
+
+
+
+
+
+*/
