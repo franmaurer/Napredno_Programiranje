6b232bdea678e6687327ac7e5ccd25d5482048ba
franmaurer <163775970+franmaurer@users.noreply.github.com>
2026-02-05 13:51:59 +0100

    Remove commented-out code and clean up formatting

--- 07_Dinamcka_memorija/Dinamicka_spremanje_var.c
+++ 07_Dinamcka_memorija/Dinamicka_spremanje_var.c
@@ -1,4 +1,3 @@
-/*
 
 #include<stdio.h>
 int main(){
@@ -13,15 +12,3 @@ int main(){
 4, broj byteove koje varijabla zauzima
 0x7fffa53aa4ec, Adresa na kojoj se nalazi varijabla a
 8, broj byteove adrese na kojoj varijabla a
-
-
-
-
-
-
-
-
-
-
-
-*/
